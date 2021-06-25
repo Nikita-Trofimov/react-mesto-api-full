c15e8d967b537f49aac67d715909c88558645622
Nikita Trofimov <nikita.igorevi4@yandex.ru>
2021-06-25 16:21:28 +0300

    настройка cookie

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,13 +6,7 @@ const jwt = require('jsonwebtoken');
 const AutorizationError = require('../utils/errors/AutorizationError');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  console.log(req.cookies.jwt);
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new AutorizationError('Необходима авторизация');
-  }
-
-  const token = authorization.replace('Bearer ', '');
+  const token = req.cookies.jwt;
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
