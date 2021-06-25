4cba0f93a34c1528376671616ab9f58249be8e77
Nikita Trofimov <nikita.igorevi4@yandex.ru>
2021-06-25 10:59:34 +0300

    изменил сервер

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
 export const token = '2e08f9f0-442c-48d2-af92-1c14d5165bb1';
-export const server = 'api.nikita-mesto.nomoredomains.club';
-export const serverAuth = 'api.nikita-mesto.nomoredomains.club';
+export const server = 'https://mesto.nomoreparties.co/v1/cohort-21';
+export const serverAuth = 'http://api.nikita-mesto.nomoredomains.club';
